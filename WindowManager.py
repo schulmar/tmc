6298a15606d8f4caabc1ca91eee24308710da43a
Martin Schulze <schulmar@hrz.tu-chemnitz.de>
2011-11-28 17:40:45 +0000

    Added doxygen comments

--- WindowManager.py
+++ WindowManager.py
@@ -2,24 +2,60 @@ from PluginInterface import *
 from Manialink import *
 from WindowElements import *
 
+"""
+\file WindowManager.py
+\brief Contains the WindowManager 
+"""
+
 class WindowManager(PluginInterface):
+	"""
+	\brief WindowManager class to abstract the Manialink interface
+	"""
 	def __init__(self, pipes, args):
-		self.displays = {}
-		super(WindowManager, self).__init__(pipes)
+		"""
+		\brief Construct the WindowManager
+		\param pipes The communication pipes to the PluginManager
+		\param args Additional startup arguments
+		"""
+		self.displays = {} #The display of each player
+		super(WindowManager, self).__init__(pipes)#initialize the PluginInterface
 
 	def initialize(self, args):
+		"""
+		\brief Initialize the WindowManager
+		\param args Should be empty
+		
+		Nothing to do
+		"""
 		pass
 
 	def displayMl(self, *args):
+		"""
+		\brief Display a manialink via ManialinkManager
+		\param args the arguments that would be passed to the ManialinkManager
+		"""
 		self.callMethod(('ManialinkManager', 'displayManialinkToLogin'), *args)
 
 	def hideMl(self, *args):
+		"""
+		\brief Hide a manialink from the dipslay of a player
+		\param args The args to hide a special manialink from the player
+		"""
 		self.callMethod(('ManialinkManager', 'hideManialinkToLogin'), *args)
 
 	def windowFrame(self, width, height, posx, posy, posz = 0):
+		"""
+		\brief ???
+		"""
 		pass
 
 	def closeWindow(self, entries, login, name):
+		"""
+		\brief Callback for closing a window
+		\param entries The entries in the window's manialink
+		\param login The login of the player
+		\param name The name of the window to close
+		"""
 		try:
 			display = self.displays[login]
 			try:
@@ -31,17 +67,38 @@ class WindowManager(PluginInterface):
 			self.log('error: login ' + str(login) + ' has no windows to be closed')
 
 	def __addWindow(self, login, name, window):
+		"""
+		\brief Add a window to manage
+		\param login The player to display the window to
+		\param name The name of the window to display
+		\param window The window instance to display
+		"""
 		if not (login in self.displays):
 			self.displays[login] = {}
 		self.displays[login][name] = window
 
 	def displayWindow(self, login, name, window):
+		"""
+		\brief Display the given window
+		\param login The player to display the window to
+		\param name The name of the window to display
+		\param window The window instance to display
+		"""
 		window.setName(name)
 		self.__addWindow(login, name, window)
 		ml = window.getManialink()
 		self.displayMl(ml, name, login)
 
 	def displayPagedWindow(self, login, name, title, size, pos, pages = []):
+		"""
+		\brief Display a paged window
+		\param login The player to display tdhe window to
+		\param name The name of the window
+		\param title The title of the window
+		\param size The size of the window
+		\param pos The upper left corner of the window
+		\param pages A list of pages (manialinks)
+		"""
 		window = PagedWindow(title, pages)
 		window.setName(name)
 		window.setSize(size)
@@ -51,6 +108,15 @@ class WindowManager(PluginInterface):
 		self.displayMl(ml, name, login)
 
 	def displayLinesWindow(self, login, name, title, size, pos, rows, rowsPerPage):
+		"""
+		\brief Display lines in a window
+		\param login The login of the player to display to
+		\param name The name of the window
+		\param size The size of the window
+		\param pos The upper left corner of the window
+		\param rows The rows to display
+		\param rowsPerPage The number of rows to display per page
+		"""
 		pages = []
 		i = 0
 		for row in rows:
@@ -69,6 +135,18 @@ class WindowManager(PluginInterface):
 
 	def displayTableWindow(self, login, name, title, size, pos, rows, rowsPerPage, 
 						columnWidths, headLine = None):
+		"""
+		\brief Display manialink elements in a table
+		\param login The player to display to
+		\param name The windows name
+		\param title The title of the window
+		\param size The size of the window
+		\param pos The upper left corner of the window
+		\param rows The rows to display (each is an iterable of columns)
+		\param rowsPerPage The number of rows to display per page
+		\param columntWidths The widths of each column
+		\param headLine The headlines of each column
+		"""
 		if headLine != None:
 			rowsPerPage += 1
 			headLineML = []
@@ -97,6 +175,18 @@ class WindowManager(PluginInterface):
 		self.displayLinesWindow(login, name, title, size, pos, lines, rowsPerPage)
 
 	def displayTableStringsWindow(self, login, name, title, size, pos, rows, rowsPerPage, columnWidths, headLine = None):
+		"""
+		\brief Display strings in a table
+		\param login The player to display to
+		\param name The windows name
+		\param title The title of the window
+		\param size The size of the window
+		\param pos The upper left corner of the window
+		\param rows The rows to display (each is an iterable of columns)
+		\param rowsPerPage The number of rows to display per page
+		\param columntWidths The widths of each column
+		\param headLine The headlines of each column
+		"""
 		lines = []
 		for r in rows:
 			line = []
@@ -115,6 +205,13 @@ class WindowManager(PluginInterface):
 
 	#args = (windowName, pageNumber)
 	def changePage(self, entries, login, name, pageNumber):
+		"""
+		\brief Change the page of a multiPage window
+		\param entries The entries of the manialink
+		\param login The login of the calling player
+		\param name The name of the window
+		\param pageNumber The number of the page to change to
+		"""
 		try:
 			display = self.displays[login]
 			try:
