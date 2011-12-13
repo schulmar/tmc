8ad7d86d1e3e5e94b5094474f229c111ad440337
Martin Schulze <schulmar@hrz.tu-chemnitz.de>
2011-12-13 19:23:11 +0000

    Moved direct map upload into own Plugin

---
+++ DirectMapUpload.py
@@ -0,0 +1,156 @@
+from PluginInterface import *
+from Manialink import *
+from WindowElements import *
+
+"""
+\file DirectUpload.py
+\brief Contains methods for direct uploads of maps
+"""
+
+class DirectMapUpload(PluginInterface):
+    """
+    \class DirectMapUpload
+    \brief Manage direct uploads of maps
+    """
+    def __init__(self, pipes, args):
+        """
+        \brief Create a new instance
+        \param pipes The communication pipes to the PluginManager
+        \param args Additional startup arguments
+        """
+        super(DirectMapUpload, self).__init__(pipes)
+        self.__directUploadPath = 'direct_upload' #the name of the map folder for direct uploads
+        
+    def initialize(self, args):
+        """
+        \brief Initialize the instance
+        \param args Additional startup arguments
+        """
+        self.callMethod(('Acl', 'rightAdd'), 'Maps.directMapUpload',
+            'Upload maps directly to the server via HTTP connection.')
+        self.callMethod(('TmChat', 'registerChatCommand'), 'upload', 
+                        ('DirectMapUpload', 'chat_upload'), 
+                        'Upload a map file to the server from within the game.')
+    
+    def chat_upload(self, login, param):
+        """
+        \brief Chat callback for upload command (display upload form)
+        \param login The login of the calling player
+        \param param Additional params, should be ignored
+        """
+        if not self.callFunction(('Acl', 'userHasRight'), login, 'Maps.directMapUpload'):
+            self.callFunction(('TmConnector', 'ChatSendServerMessageToLogin'), 
+                            'You have insufficient rights to directly upload maps to this server!', 
+                            login)
+            return False
+        
+        frame = Frame()
+        
+        #the label of the submit button
+        label = Label()
+        label['posn'] = '19 -1'
+        label['text'] = 'Upload!'
+        frame.addChild(label)
+        #the submit butten background
+        quad = Quad()
+        quad['posn'] = '18 0'
+        quad['sizen'] = '9 4'
+        quad['style'] = 'Bgs1'
+        quad['substyle'] = 'PlayerCard'
+        ml = self.callFunction(('Http', 'getUploadToken'), 
+                               ('DirectMapUpload', 'directMapUpload'), 
+                               login)
+        quad['manialink'] = ('POST(http://' + str(ml[1][0]) + ':' + str(ml[1][1]) 
+                                + '?token=' + str(ml[0]) + 
+                                '&map=inputTrackFile,inputTrackFile)')
+        frame.addChild(quad)
+        
+        #the entry
+        entry = FileEntry()
+        entry['posn'] = "2 -1"
+        entry['sizen'] = "15 3"
+        entry['name'] = "inputTrackFile"
+        entry['folder'] = "Maps"
+        entry['default'] = "Pick Map"
+        frame.addChild(entry)
+        
+        w = Window('Choose map for upload')
+        w.setSize((30, 10))
+        w.setPos((-15, 5))
+        w.addChild(frame)
+        
+        self.callMethod(('WindowManager', 'displayWindow'), 
+                    login, 'DirectMapUpload.directMapUpload', w)
+        
+    def directMapUpload(self, entries, data, login):
+        """
+        \brief Callback for the direct http upload of maps
+        \param entries The entries dict of the request (containing the filename)
+        \param data The file data of the file uploaded
+        \param login The login of the uploading user
+        """
+        #hide the upload form as the token is now expired
+        self.callMethod(('ManialinkManager', 'hideManialinkToLogin'), 
+                        'DirectMapUpload.directMapUpload', login)
+        if self.callFunction(('Acl', 'userHasRight'), login, 
+                             'DirectMapUpload.directMapUpload'):
+            #get the map folder path
+            mapPath = (os.path.dirname(self.callFunction(
+                                    ('TmConnector', 'GetMapsDirectory')))
+                        + os.path.sep)
+            #get the filename
+            fileName = os.path.basename(entries['map'])
+            #assemble the relative path for the users map files
+            relPath = self.__directUploadPath + os.path.sep + login + os.path.sep
+            #create dir if not already existent
+            if not os.path.isdir(mapPath + relPath):
+                os.mkdir(mapPath + relPath)
+            #test if this file already exists
+            if os.path.isfile(mapPath + relPath + fileName):
+                return """
+                    <?xml version="1.0" encoding="utf-8" ?>
+                    <manialink>
+                        <label text="$f11$oError$o$fff: This file already exists!"/>
+                    </manialink>
+                    """
+            #try to write the file
+            try:
+                mapFile = open(mapPath + relPath + fileName, "w")
+                mapFile.write(data)
+                mapFile.close()
+            except:
+                return """
+                    <?xml version="1.0" encoding="utf-8" ?>
+                    <manialink>
+                        <label text="$f11$oError$o$fff: Could not write file. Try again later!"/>
+                    </manialink>
+                    """
+                    
+            if self.callFunction(('TmConnector', 'AddMap'), relPath + fileName):
+                info = self.callFunction(('TmConnector', 'GetMapInfo'), relPath + fileName)
+                return """
+                <?xml version="1.0" encoding="utf-8" ?>
+                <manialink>
+                    <label text="Thank you for uploading this map!"/>
+                </manialink>
+                """
+                self.callMethod(('TmConnector', 'ChatSendServerMessageToLogin'),
+                            '$zAdded map ' + info['Name'] + ' $zto list!', login)
+            else:
+                os.remove(mapPath + relPath + fileName)
+                return """
+                <?xml version="1.0" encoding="utf-8" ?>
+                <manialink>
+                    <label text="Could not add map to list. Is this a map file?"/>
+                </manialink>
+                """
+        else:
+            self.callFunction(('TmConnector', 'ChatSendServerMessageToLogin'), 
+                            'You have insufficient rights to directly upload maps to this server!', 
+                            login)
+            return """
+                    <?xml version="1.0" encoding="utf-8" ?>
+                    <manialink>
+                        <label text="You have insufficient rights to directly upload maps to this server!"/>
+                    </manialink>
+                    """
--- Maps.py
+++ Maps.py
@@ -39,7 +39,6 @@ class Maps(PluginInterface):
 		self.__matchSettingsFileName = 'tracklist.txt' #The name of the matchsettings file
 		self.__jukebox = [] #List of tracks that are currently in the jukebox (track, loginOfJuker)
 		self.__playerDict = {} #a dictionary that contains the index mapping of the tracks per user
-		self.__directUploadPath = 'direct_upload' #the name of the map folder for direct uploads
 		super(Maps, self).__init__(pipes)
 
 	def initialize(self, args):
@@ -105,13 +104,6 @@ class Maps(PluginInterface):
 					'Display the jukebox')
 		self.callMethod(('TmChat', 'registerChatCommand'), 'jukebox', ('Maps', 'chat_jukebox'),
 					'Jukebox mapmanagement. Type /jukebox help for more information')
-		
-		self.callMethod(('Acl', 'rightAdd'), 'Maps.directMapUpload',
-					'Upload maps directly to the server via HTTP connection.')
-		self.callMethod(('TmChat', 'registerChatCommand'), 'upload', ('Maps', 'chat_upload'), 
-					'Upload a map file to the server from within the game.')
-		self.callMethod(('Acl', 'rightAdd'), 'Maps.directMapUpload', 
-					'Directly upload maps via HTTP to the server.')
 		
 		self.callMethod(('TmChat', 'registerChatCommand'), 'karma', ('Maps', 'chat_karma'),
 					'Handle the karma of this map.')
@@ -619,122 +611,6 @@ class Maps(PluginInterface):
 			#only raised when this player never used the jukebox
 			pass
 		
-	def chat_upload(self, login, param):
-		"""
-		\brief Chat callback for upload command (display upload form)
-		\param login The login of the calling player
-		\param param Additional params, should be ignored
-		"""
-		if not self.callFunction(('Acl', 'userHasRight'), login, 'Maps.directMapUpload'):
-			self.callFunction(('TmConnector', 'ChatSendServerMessageToLogin'), 
-							'You have insufficient rights to directly upload maps to this server!', 
-							login)
-			return False
-		
-		frame = Frame()
-		
-		#the label of the submit button
-		label = Label()
-		label['posn'] = '19 -1'
-		label['text'] = 'Upload!'
-		frame.addChild(label)
-		#the submit butten background
-		quad = Quad()
-		quad['posn'] = '18 0'
-		quad['sizen'] = '9 4'
-		quad['style'] = 'Bgs1'
-		quad['substyle'] = 'PlayerCard'
-		ml = self.callFunction(('Http', 'getUploadToken'), ('Maps', 'directMapUpload'), login)
-		quad['manialink'] = 'POST(http://' + str(ml[1][0]) + ':' + str(ml[1][1]) \
-		                                        + '?token=' + str(ml[0]) + '&map=inputTrackFile,inputTrackFile)'
-		frame.addChild(quad)
-		
-		#the entry
-		entry = FileEntry()
-		entry['posn'] = "2 -1"
-		entry['sizen'] = "15 3"
-		entry['name'] = "inputTrackFile"
-		entry['folder'] = "Maps"
-		entry['default'] = "Pick Map"
-		frame.addChild(entry)
-		
-		w = Window('Choose map for upload')
-		w.setSize((30, 10))
-		w.setPos((-15, 5))
-		w.addChild(frame)
-		
-		self.callMethod(('WindowManager', 'displayWindow'), 
-					login, 'Maps.directMapUpload', w)
-		
-	def directMapUpload(self, entries, data, login):
-		"""
-		\brief Callback for the direct http upload of maps
-		\param entries The entries dict of the request (containing the filename)
-		\param data The file data of the file uploaded
-		\param login The login of the uploading user
-		"""
-		#hide the upload form as the token is now expired
-		self.callMethod(('ManialinkManager', 'hideManialinkToLogin'), 'Maps.directMapUpload', login)
-		if self.callFunction(('Acl', 'userHasRight'), login, 'Maps.directMapUpload'):
-			#get the map folder path
-			mapPath = (os.path.dirname(self.callFunction(('TmConnector', 'GetMapsDirectory')))
-						+ os.path.sep)
-			#get the filename
-			fileName = os.path.basename(entries['map'])
-			#assemble the relative path for the users map files
-			relPath = self.__directUploadPath + os.path.sep + login + os.path.sep
-			#create dir if not already existent
-			if not os.path.isdir(mapPath + relPath):
-				os.mkdir(mapPath + relPath)
-			#test if this file already exists
-			if os.path.isfile(mapPath + relPath + fileName):
-				return """
-					<?xml version="1.0" encoding="utf-8" ?>
-					<manialink>
-						<label text="$f11$oError$o$fff: This file already exists!"/>
-					</manialink>
-					"""
-			#try to write the file
-			try:
-				mapFile = open(mapPath + relPath + fileName, "w")
-				mapFile.write(data)
-				mapFile.close()
-			except:
-				return """
-					<?xml version="1.0" encoding="utf-8" ?>
-					<manialink>
-						<label text="$f11$oError$o$fff: Could not write file. Try again later!"/>
-					</manialink>
-					"""
-					
-			if self.callFunction(('TmConnector', 'AddMap'), relPath + fileName):
-				info = self.callFunction(('TmConnector', 'GetMapInfo'), relPath + fileName)
-				return """
-				<?xml version="1.0" encoding="utf-8" ?>
-				<manialink>
-					<label text="Thank you for uploading this map!"/>
-				</manialink>
-				"""
-				self.callMethod(('TmConnector', 'ChatSendServerMessageToLogin'),
-							'$zAdded map ' + info['Name'] + ' $zto list!', login)
-			else:
-				os.remove(mapPath + relPath + fileName)
-				return """
-				<?xml version="1.0" encoding="utf-8" ?>
-				<manialink>
-					<label text="Could not add map to list. Is this a map file?"/>
-				</manialink>
-				"""
-		else:
-			self.callFunction(('TmConnector', 'ChatSendServerMessageToLogin'), 
-							'You have insufficient rights to directly upload maps to this server!', 
-							login)
-			return """
-					<?xml version="1.0" encoding="utf-8" ?>
-					<manialink>
-						<label text="You have insufficient rights to directly upload maps to this server!"/>
-					</manialink>
-					"""
 	def chat_karma(self, login, params):
 		"""
 		\brief Handle the karma commands for the current map
