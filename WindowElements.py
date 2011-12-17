50e2f6de81d32d36db818d0c40b99a8ab36f0b18
Martin Schulze <schulmar@hrz.tu-chemnitz.de>
2011-12-17 10:35:56 +0000

    again :(

--- WindowElements.py
+++ WindowElements.py
@@ -767,7 +767,7 @@ class CommentOutput(PagedWindow):
         if deleted:
             height = 0
         else:
-            height = len(['commentTuple'][1]) // width
+            height = len(comment['commentTuple'][1]) // width
         
         consumedHeight = 0
         
